0a9ae51130fb14eab88874831e5d05c25f47f7de
Simon Ha <simon.ha@kcl.ac.uk>
2023-10-10 22:05:03 +0100

    Made Thing model

--- things/models.py
+++ things/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+class Thing(models.Model):
+    name = models.CharField(
+        max_length=10, 
+        unique=True,
+    )
+    description = models.CharField(
+        max_length=20, 
+    )
+    quantity = models.IntegerField()
+
