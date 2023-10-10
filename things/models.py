7a2082b85e53c42258b0c41b392a042d658a16c8
Simon Ha <simon.ha@kcl.ac.uk>
2023-10-10 22:12:58 +0100

    Add constraints to model

--- things/models.py
+++ things/models.py
@@ -1,12 +1,24 @@
 from django.db import models
+from django.core.exceptions import ValidationError
+
+def validate_quantity(value):
+    if value <= 0 and value >= 100:
+        raise(ValidationError("Quantity needs to be a value from 0 to 100!"))
 
 class Thing(models.Model):
     name = models.CharField(
-        max_length=10, 
+        max_length=30, 
         unique=True,
+        blank=False
     )
     description = models.CharField(
-        max_length=20, 
+        max_length=120, 
+        unique=False,
+        blank=True
     )
-    quantity = models.IntegerField()
+    quantity = models.IntegerField(
+        unique=False,
+        validators=[validate_quantity]
+    )
+
 
